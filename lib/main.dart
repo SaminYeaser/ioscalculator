c45e1ac0d48f2768c65e4db3370e337f4a5fbc5d
saminyeaser <saminyeaser1@gmail.com>
2020-06-11 12:14:25 +0600

    working...2

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(IOSCalculator());
 }
 
@@ -17,13 +16,13 @@ class IOSCalculator extends StatelessWidget {
     );
   }
 }
+
 class IosCal extends StatefulWidget {
   @override
   _IosCalState createState() => _IosCalState();
 }
 
 class _IosCalState extends State<IosCal> {
-
   dynamic text = '0';
   dynamic numOne = 0;
   dynamic numTwo = 0;
@@ -32,6 +31,7 @@ class _IosCalState extends State<IosCal> {
   dynamic finalResult = '';
   dynamic opr = '';
   dynamic preOpr = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,75 +41,89 @@ class _IosCalState extends State<IosCal> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               Expanded(
-                child: Text(text,
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60,
-                ),),
+                child: SingleChildScrollView(
+                  child: Text(
+                    text,
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                    ),
+                  ),
+                ),
               )
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('C', Colors.grey, Colors.black),
-              button('+/-', Colors.grey,Colors.black),
-              button('%', Colors.grey,Colors.black),
-              button('\u00F7', Colors.orange,Colors.white)
-            ],
+          Container(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    button('C', Colors.grey, Colors.black),
+                    button('+/-', Colors.grey, Colors.black),
+                    button('%', Colors.grey, Colors.black),
+                    button('\u00F7', Colors.orange, Colors.white)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    button('7', Colors.grey.shade900, Colors.white),
+                    button('8', Colors.grey.shade900, Colors.white),
+                    button('9', Colors.grey.shade900, Colors.white),
+                    button('\u00D7', Colors.orange, Colors.white)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    button('4', Colors.grey.shade900, Colors.white),
+                    button('5', Colors.grey.shade900, Colors.white),
+                    button('6', Colors.grey.shade900, Colors.white),
+                    button('-', Colors.orange, Colors.white)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    button('1', Colors.grey.shade900, Colors.white),
+                    button('2', Colors.grey.shade900, Colors.white),
+                    button('3', Colors.grey.shade900, Colors.white),
+                    button('+', Colors.orange, Colors.white)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    buttonZero('0', Colors.grey.shade900, Colors.white),
+                    button('.', Colors.grey.shade900, Colors.white),
+                    button('=', Colors.grey.shade900, Colors.white),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('7', Colors.grey.shade900,Colors.white),
-              button('8', Colors.grey.shade900, Colors.white),
-              button('9', Colors.grey.shade900,Colors.white),
-              button('\u00D7', Colors.orange,Colors.white)
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('4', Colors.grey.shade900,Colors.white),
-              button('5', Colors.grey.shade900,Colors.white),
-              button('6', Colors.grey.shade900,Colors.white),
-              button('-', Colors.orange,Colors.white)
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('1', Colors.grey.shade900,Colors.white),
-              button('2', Colors.grey.shade900,Colors.white),
-              button('3', Colors.grey.shade900,Colors.white),
-              button('+', Colors.orange,Colors.white)
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buttonZero('0', Colors.grey.shade900,Colors.white),
-              button('.', Colors.grey.shade900,Colors.white),
-              button('=', Colors.grey.shade900,Colors.white),
-            ],
-          )
+
         ],
       ),
     );
   }
-  Widget button(text, Color color, txtColor){
+
+  Widget button(text, Color color, txtColor) {
     return Container(
       padding: EdgeInsets.only(bottom: 10),
       child: RaisedButton(
-        child: Text(text,
-        style: TextStyle(
-          color: txtColor,
-          fontSize: 20,
-        ),),
-        onPressed: (){
+        child: Text(
+          text,
+          style: TextStyle(
+              color: txtColor, fontSize: 25, fontWeight: FontWeight.w900),
+        ),
+        onPressed: () {
           calculation(text);
         },
         color: color,
@@ -118,16 +132,19 @@ class _IosCalState extends State<IosCal> {
       ),
     );
   }
-  Widget buttonZero(text, Color color, txtColor){
+
+  Widget buttonZero(text, Color color, txtColor) {
     return Container(
       child: RaisedButton(
-        child: Text(text,
+        child: Text(
+          text,
           style: TextStyle(
             color: txtColor,
             fontSize: 20,
-          ),),
-        onPressed: (){
-        calculation(text);
+          ),
+        ),
+        onPressed: () {
+          calculation(text);
         },
         color: color,
         padding: EdgeInsets.only(left: 90, right: 85, top: 20, bottom: 20),
@@ -135,79 +152,80 @@ class _IosCalState extends State<IosCal> {
       ),
     );
   }
-  void calculation(btntxt){
-    if(btntxt == 'C'){
-       text = '0';
-       numOne = 0;
-       numTwo = 0;
 
-       result = '';
-       finalResult = '';
-       opr = '';
-       preOpr = '';
-    }
+  void calculation(btntxt) {
+    if (btntxt == 'C') {
+      text = '0';
+      numOne = 0;
+      numTwo = 0;
 
-
-    else if(opr == '=' && preOpr == '='){
-      if(preOpr == '+'){
+      result = '';
+      finalResult = '';
+      opr = '';
+      preOpr = '';
+    } else if (btntxt == '+/-') {
+      finalResult = 'Currently working on this!!';
+    } else if (opr == '=' && preOpr == '=') {
+      if (preOpr == '+') {
         result = (numOne + numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(preOpr == '\u00D7' || preOpr == '*'){
+      } else if (preOpr == '\u00D7' || preOpr == '*') {
         result = (numOne * numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(preOpr == '\u00F7' || preOpr == '/'){
+      } else if (preOpr == '\u00F7' || preOpr == '/') {
         result = (numOne / numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(preOpr == '-'){
+      } else if (preOpr == '-') {
         result = (numOne - numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(preOpr == '%'){
-        result = {numOne * (numTwo/100)}.toString();
+      } else if (preOpr == '%') {
+        result = (numOne * numTwo / 100).toString();
         finalResult = result;
         numOne = double.parse(result);
       }
-    }
-
-
-    else if(btntxt == '+' || btntxt == '-' || btntxt == '/' || btntxt == '*' || btntxt =='\u00F7' || btntxt == '\u00D7' || btntxt == '=' || btntxt == '%'){
-      if(numOne == 0){
+    } else if (btntxt == '+' ||
+        btntxt == '-' ||
+        btntxt == '/' ||
+        btntxt == '*' ||
+        btntxt == '\u00F7' ||
+        btntxt == '\u00D7' ||
+        btntxt == '=' ||
+        btntxt == '%' ||
+        btntxt == '+/-') {
+      if (numOne == 0) {
         numOne = double.parse(result);
-      }else{
+      } else {
         numTwo = double.parse(result);
       }
-      if(opr == '+'){
+      if (opr == '+') {
         result = (numOne + numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }
-      else if(opr == '\u00D7' || opr == '*'){
+      } else if (opr == '\u00D7' || opr == '*') {
         result = (numOne * numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }
-      else if(opr == '\u00F7' || opr == '/'){
-        result = (numOne/numTwo).toString();
+      } else if (opr == '\u00F7' || opr == '/') {
+        result = (numOne / numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(opr == '-'){
+      } else if (opr == '-') {
         result = (numOne - numTwo).toString();
         finalResult = result;
         numOne = double.parse(result);
-      }else if(opr == '%'){
-        result = {numOne * (numTwo/100)}.toString();
+      } else if (opr == '%') {
+        result = (numOne * numTwo / 100).toString();
         finalResult = result;
         numOne = double.parse(result);
       }
       preOpr = opr;
       opr = btntxt;
       result = '';
-    }
-
-    else{
+    } else {
       result = result + btntxt;
       finalResult = result;
     }
@@ -217,4 +235,3 @@ class _IosCalState extends State<IosCal> {
     });
   }
 }
-
